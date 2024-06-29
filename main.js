e55d43282b2946650dc1a7fe0ce7d6e9c4c7ebd4
Pk322 <160255216+Pk322@users.noreply.github.com>
2024-06-29 21:10:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//Assignment No:03
+/*3.Famous Quote: Find a quote from a famous person you admire.
+ Print the quote and the name of its author. Your output should look
+  something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never
+tried anything new.”*/
+// storing the famous quote & its author
+var quote = "A person who never made a mistake never tried anything new.";
+var author = "Albert Einstein";
+//Print the quote and the name of its author
+console.log("".concat(author, " once said,\"").concat(quote, "\""));
---
+++ main.ts
@@ -0,0 +1,19 @@
+//Assignment No:03
+
+/*3.Famous Quote: Find a quote from a famous person you admire.
+ Print the quote and the name of its author. Your output should look
+  something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never 
+tried anything new.”*/
+
+// storing the famous quote & its author
+
+let quote : string ="A person who never made a mistake never tried anything new.";
+
+let author : string ="Albert Einstein";
+
+//Print the quote and the name of its author
+
+ console.log(`${author} once said,"${quote}"`);
+
+
